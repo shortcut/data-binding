119944b5fd112ff8f7ee7d3fd7e68cf129ce1f35
Cyrus Bakhtiari-Haftlang <cyrus.bh@gmail.com>
2020-03-29 20:23:51 +0200

    Added transition target includes and excludes

--- library/src/main/java/se/ingenuity/databinding/adapter/ViewBindingAdapter.kt
+++ library/src/main/java/se/ingenuity/databinding/adapter/ViewBindingAdapter.kt
@@ -10,39 +10,71 @@ import se.ingenuity.databinding.extension.findParentById
 
 object ViewBindingAdapter {
     @JvmStatic
-    @BindingAdapter(value = ["visibleOrGone", "transition", "sceneRoot"], requireAll = false)
+    @BindingAdapter(
+        value = [
+            "visibleOrGone",
+            "transition",
+            "transitionRoot",
+            "transitionIncludeTargets",
+            "transitionExcludeTargets"
+        ],
+        requireAll = false
+    )
     fun View.setVisibleOrGone(
         visible: Boolean,
         transition: Transition?,
-        @IdRes sceneRoot: Int?
+        @IdRes transitionRoot: Int?,
+        includeTargets: List<Int>?,
+        excludeTargets: List<Int>?
     ) {
-        beginDelayedTransition(transition, sceneRoot)
+        beginDelayedTransition(transition, transitionRoot, includeTargets, excludeTargets)
 
         visibility = if (visible) View.VISIBLE else View.GONE
     }
 
     @JvmStatic
-    @BindingAdapter(value = ["visibleOrInvisible", "transition", "sceneRoot"], requireAll = false)
+    @BindingAdapter(
+        value = [
+            "visibleOrInvisible",
+            "transition",
+            "transitionRoot",
+            "transitionIncludeTargets",
+            "transitionExcludeTargets"
+        ],
+        requireAll = false
+    )
     fun View.setVisibleOrInvisible(
         visible: Boolean,
         transition: Transition?,
-        @IdRes sceneRoot: Int?
+        @IdRes transitionRoot: Int?,
+        includeTargets: List<Int>?,
+        excludeTargets: List<Int>?
     ) {
-        beginDelayedTransition(transition, sceneRoot)
+        beginDelayedTransition(transition, transitionRoot, includeTargets, excludeTargets)
 
         visibility = if (visible) View.VISIBLE else View.INVISIBLE
     }
 
     private fun View.beginDelayedTransition(
         transition: Transition?,
-        @IdRes sceneRoot: Int?
+        @IdRes transitionRoot: Int?,
+        includeTargets: List<Int>?,
+        excludeTargets: List<Int>?
     ) {
         if (transition != null) {
-            val root = if (sceneRoot != null) {
-                findParentById(sceneRoot)
+            val root = if (transitionRoot != null) {
+                findParentById(transitionRoot)
             } else {
                 parent as View
             } ?: parent as View
+
+            includeTargets?.forEach { targetId ->
+                transition.addTarget(targetId)
+            }
+
+            excludeTargets?.forEach { targetId ->
+                transition.excludeTarget(targetId, true)
+            }
 
             TransitionManager.beginDelayedTransition(root as ViewGroup, transition)
         }
